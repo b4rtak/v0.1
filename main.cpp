c6b7cabfd62b99ba5361398228ba0840f5ee7f24
Mantas <mantas.jakimavicius17@gmail.com>
2026-09-22 21:02:27 +0300

    Studento struktura

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    vector<int> nd; //nd rez
+    int egzaminas;
+    double galutinis;
+};
+
+int main() {
+    vector<studentas> studentai;
+
+    return 0;
+};
