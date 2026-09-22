09b7ebf382b835f49d55c0bf00acaf14298b8066
Mantas <mantas.jakimavicius17@gmail.com>
2026-09-22 22:46:51 +0300

    Duomenu ivedimas

--- main.cpp
+++ main.cpp
@@ -15,6 +15,33 @@ struct studentas {
 
 int main() {
     vector<studentas> studentai;
+    int kiekis;
+    cout << "Kiek studentu?: ";
+    cin >> kiekis;
+
+    for (int i = 0; i < kiekis; i++) {
+        studentas s;
+        cout << "\n" << i + 1 << "-as studentas\n";
+        cout << "Vardas: ";
+        cin >> s.vardas;
+        cout << "Pavarde: ";
+        cin >> s.pavarde;
+
+        int n;
+        cout << "Kiek namu darbu?: ";
+        cin >> n;
+        for (int j = 0; j < n; j++) {
+            int pazymys;
+            cout << j + 1 << "-o namu darbo rezultatas: ";
+            cin >> pazymys;
+            s.nd.push_back(pazymys);
+        }
+
+        cout << "Egzamino rezultatas: ";
+        cin >> s.egzaminas;
+
+        studentai.push_back(s);
+    }
 
     return 0;
 };
