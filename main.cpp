95ee7f09b1017f3c6caf47ef2f6322206b8790e8
Mantas <mantas.jakimavicius17@gmail.com>
2026-09-23 18:21:39 +0300

    Funkcijos, random generavimas ir meniu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
+#include <string>
 #include <vector>
 #include <algorithm>
+#include <random>
 #include <limits>
 
 using namespace std;
@@ -10,16 +11,16 @@ using namespace std;
 struct studentas {
     string vardas;
     string pavarde;
-    vector<int> nd; //nd rez
-    int egzaminas;
+    vector<int> nd;     // namu darbu rezultatai
+    int egzaminas = 0;
     double galVid = 0, galMed = 0;
 };
 
-double vidurkis(const vector<int>& pazymiai) {
-    if (pazymiai.empty()) return 0.0;
+double vidurkis(const vector<int>& v) {
+    if (v.empty()) return 0.0;
     double suma = 0;
-    for (int p : pazymiai) suma += p;
-    return suma / pazymiai.size();
+    for (int p : v) suma += p;
+    return suma / v.size();
 }
 
 double mediana(vector<int> v) {
@@ -30,41 +31,57 @@ double mediana(vector<int> v) {
     return v[n / 2];
 }
 
-int main() {
-    vector<studentas> studentai;
-    int kiekis;
-    cout << "Kiek studentu?: ";
-    cin >> kiekis;
+void skaiciuoti(studentas& s) {
+    s.galVid = 0.4 * vidurkis(s.nd) + 0.6 * s.egzaminas;
+    s.galMed = 0.4 * mediana(s.nd) + 0.6 * s.egzaminas;
+}
 
-    for (int i = 0; i < kiekis; i++) {
-        studentas s;
-        cout << "\n" << i + 1 << "-as studentas\n";
-        cout << "Vardas: ";
-        cin >> s.vardas;
-        cout << "Pavarde: ";
-        cin >> s.pavarde;
-
+Studentas ivestistudenta(bool generuoti, mt19937& gen) {
+    Studentas s;
+    cout << "Vardas: ";
+    cin >> s.vardas;
+    cout << "Pavarde: ";
+    cin >> s.pavarde;
+    if (generuoti) {
+        uniform_int_distribution<int> balas(1, 10);
+        int n = balas(gen);
+        for (int i = 0; i < n; i++) s.nd.push_back(balas(gen));
+        s.egzaminas = balas(gen);
+    } else {
         cout << "ND rezultatai (po viena eiluteje, baigti - tuscia eilute):\n";
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         string eil;
         while (getline(cin, eil) && !eil.empty())
             s.nd.push_back(stoi(eil));
-        
-
         cout << "Egzamino rezultatas: ";
         cin >> s.egzaminas;
-
-        s.galVid = 0.4 * vidurkis(s.nd) + 0.6 * s.egzaminas;
-        s.galMed = 0.4 * mediana(s.nd) + 0.6 * s.egzaminas;
-        studentai.push_back(s);
     }
+    skaiciuoti(s);
+    return s;
+}
 
-    cout << "\n" << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (vid.)" << "Galutinis (med.)\n";
-    cout << string(76, '-') << "\n" << fixed << setprecision(2);
-    for (const auto& s : studentai) {
-        cout << left << setw(20) << s.pavarde << setw(20) << s.vardas << setw(20) << s.galVid << s.galMed << "\n";
+void spausdinti(vector<Studentas>& studentai, ostream& out) {
+    out << left << setw(20) << "Pavarde" << setw(20) << "Vardas"
+        << setw(20) << "Galutinis (vid.)" << "Galutinis (med.)\n";
+    out << string(76, '-') << "\n" << fixed << setprecision(2);
+    for (const auto& s : studentai)
+        out << left << setw(20) << s.pavarde << setw(20) << s.vardas
+            << setw(20) << s.galVid << s.galMed << "\n";
+}
+
+int main() {
+    vector<studentas> studentai;
+    mt19937 gen(random_device{}());
+    while (true) {
+        cout << "\n1 - Ivesti studenta ranka\n2 - Generuoti pazymius atsitiktinai\n"
+             << "3 - Rodyti rezultatus\n0 - Baigti\nPasirinkimas: ";
+        int pas;
+        cin >> pas;
+        if (pas == 0) break;
+        if (pas == 1 || pas == 2) studentai.push_back(ivestistudenta(pas == 2, gen));
+        else if (pas == 3) spausdinti(studentai, cout);
+        else cout << "Tokio pasirinkimo nera\n";
     }
-
     return 0;
-};
+}
 
